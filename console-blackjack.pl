c207832b4685c60c97d3dcaaa13986b878399b35
Greg Donald <gdonald@gmail.com>
2020-05-17 14:46:35 -0500

    player_can_hit

--- console-blackjack.pl
+++ console-blackjack.pl
@@ -3,6 +3,7 @@
 use v5.10;
 use strict;
 use warnings FATAL => 'all';
+use Data::Dumper;
 
 use constant {
   HARD => 'hard',
@@ -37,13 +38,13 @@ sub is_ten {
 }
 
 sub player_get_value {
-  my ($hand, $method) = @_;
+  my ($player_hand, $method) = @_;
 
   my $v = 0;
   my $total = 0;
   my $tmp_v = 0;
 
-  for (@$hand) {
+  for (@{$player_hand->{'hand'}}) {
     $tmp_v = $_->{'value'} + 1;
     $v = $tmp_v > 9 ? 10 : $tmp_v;
 
@@ -55,16 +56,16 @@ sub player_get_value {
   }
 
   if ($method eq SOFT && $total > 21) {
-    return player_get_value($hand, HARD);
+    return player_get_value($player_hand, HARD);
   }
 
   $total;
 }
 
 sub player_is_busted {
-  my ($hand) = @_;
+  my ($player_hand) = @_;
 
-  player_get_value($hand, SOFT) > 21 ? 1 : 0;
+  player_get_value($player_hand, SOFT) > 21 ? 1 : 0;
 }
 
 sub is_blackjack {
@@ -78,16 +79,24 @@ sub is_blackjack {
     return 1;
   }
 
-  if (is_ace(@$hand[1]) && is_ten(@$hand[0])) {
-    return 1;
-  }
-
-  return 0;
+  is_ace(@$hand[1]) && is_ten(@$hand[0]) ? 1 : 0;
 }
 
-my %card_1 = ('suit' => 2, 'value' => 9);
-my %card_2 = ('suit' => 2, 'value' => 0);
+sub player_can_hit {
+  my ($player_hand) = @_;
+
+  ($player_hand->{'played'}
+    || $player_hand->{'stood'}
+    || 21 == player_get_value($player_hand, HARD)
+    || is_blackjack($player_hand->{'hand'})
+    || player_is_busted($player_hand)) ? 0 : 1;
+}
+
+my %card_1 = (suit => 2, value => 2);
+my %card_2 = (suit => 2, value => 4);
 
 my @hand = (\%card_1, \%card_2);
 
-say is_blackjack(\@hand);
+my %player_hand = (hand => [@hand], played => 0, stood => 0);
+
+say player_can_hit(\%player_hand);
